6a1fa4f9646085519f4cdfebee1f58da0bc1c690
GEERUP <GEERUP>
2018-05-28 13:19:00 +0900

    Merge branch 'master' of C:\Users\kyuhw\StudioProjects\CustomDragKeyboard with conflicts.

--- app/src/main/java/org/dyndns/fules/ck/MainActivity.java
+++ app/src/main/java/org/dyndns/fules/ck/MainActivity.java
@@ -1,8 +1,12 @@
 package org.dyndns.fules.ck;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn1_click(View v) {
+        Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+        startActivityForResult(intent, 0);
+    }
+
+    public void btn2_click(View v) {
+        InputMethodManager imeManager = (InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+        imeManager.showInputMethodPicker();
     }
 }
